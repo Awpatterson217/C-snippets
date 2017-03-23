71194b908ce758be5816edaa259a01dbec0fbcfc
Adam Patterson <awpatterson217@gmail.com>
2017-03-22 20:02:33 -0500

    Update forloopcountdown.c

--- forloopcountdown.c
+++ forloopcountdown.c
@@ -7,10 +7,9 @@ int main()
 	int y;
 	printf( "Pick a number to countdown from:\n" );
 	scanf( "%d", &x );
-
 	for ( x; x>=0; x-- )
 	{
-	printf( "%d\n", x );
+	    printf( "%d\n", x );
 	}
 	return 0;
 }
