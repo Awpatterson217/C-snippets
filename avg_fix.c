848ab32b62c2d605e95bae586b52009af8936e6b
Adam Patterson <awpatterson217@gmail.com>
2017-03-22 20:01:09 -0500

    Update avg_fix.c

--- avg_fix.c
+++ avg_fix.c
@@ -5,19 +5,15 @@ int main(void)
 {
 	int i, numberOfNums = 0, total = 0, value;
 	float average;
-
 	printf( "How many numbers are to be read?" );
 	scanf( "%d", &numberOfNums );
-
 	for( i=0; i<numberOfNums; i++ )
 	{
-	scanf( "%d", &value );
-	total += value; 
-	printf( "Read %d\n" ,value);
+	    scanf( "%d", &value );
+	    total += value; 
+	    printf( "Read %d\n" ,value);
 	}
-
 	average = (float)total / (float)numberOfNums;
 	printf( "You read %d values. Average = %f.\n", numberOfNums, average );
-	
 	return 0;
 }
